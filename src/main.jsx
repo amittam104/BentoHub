bb3d15def039f0f0a1820dbfafd6dbea610060e2
Amit Tambulkar <amittambulkar104@gmail.com>
2024-06-29 19:39:15 +0530

    chore: add posthog analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import posthog from "posthog-js";
+
+posthog.init(import.meta.env.VITE_PH_API_KEY, {
+  api_host: import.meta.env.VITE_PH_API_HOST,
+  person_profiles: "identified_only",
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
