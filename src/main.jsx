5a972f37fb19f22f37bcbaf8cde76cfa0130f669
Amit Tambulkar <amittambulkar104@gmail.com>
2024-06-29 20:39:12 +0530

    fix: update posthog config

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
-posthog.init(import.meta.env.VITE_PH_API_KEY, {
+const options = {
   api_host: import.meta.env.VITE_PH_API_HOST,
-  person_profiles: "identified_only",
-});
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider apiKey={import.meta.env.VITE_PH_API_KEY} options={options}>
+      <App />
+    </PostHogProvider>
   </React.StrictMode>,
 );
